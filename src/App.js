6af209ad6b7f6fe3f5a944c96e76145b2feb56bf
¨Elvalen0010¨ <valentinvernaz@gmail.com>
2024-02-05 21:35:48 -0300

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Section1 from './components/section1';
+import Section2 from './components/section2';
+import Section3 from './components/section3';
+import Section4 from './components/section4';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container">
+            <Link className="navbar-brand" to="/">Mi Sitio</Link>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/seccion1">Sección 1</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/seccion2">Sección 2</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/seccion3">Sección 3</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/seccion4">Sección 4</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<h2>Bienvenido a la Página Principal</h2>} />
+          <Route path="/seccion1" element={<Section1 />} />
+          <Route path="/seccion2" element={<Section2 />} />
+          <Route path="/seccion3" element={<Section3 />} />
+          <Route path="/seccion4" element={<Section4 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/section4.js
@@ -0,0 +1,13 @@
+// Section1.js
+import React from 'react';
+
+const Section4 = () => {
+  return (
+    <div>
+      <h2>Contenido de la Sección 1</h2>
+      {/* Agrega aquí el contenido específico de la Sección 1 */}
+    </div>
+  );
+};
+
+export default Section4;
